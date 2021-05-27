0b9283a52b71db8928338b2b97d0eab2b5bcdb07
apiwit <axon.apiwit@gmail.com>
2021-05-27 23:35:28 +0700

    fix announce service

--- src/modules/announce/announce.service.ts
+++ src/modules/announce/announce.service.ts
@@ -107,7 +107,7 @@ export class AnnounceService extends BaseService<AnnounceDocument> {
             },
             email: {
               $regex: arg.email,
-            },
+            }
           },
         ],
       },
